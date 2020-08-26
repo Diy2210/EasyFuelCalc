cdcb5d76a492ea96f1188925301c1ca9d78d6f6a
Dmitriy <diy2210@gmail.com>
2020-08-26 16:35:52 +0300

    fix app icon; add SettingsModel;

---
+++ app/src/main/java/net/multipul/easyfuelcalc/model/SettingsModel.kt
@@ -0,0 +1,9 @@
+package net.multipul.easyfuelcalc.model
+
+data class SettingsModel (
+//    val language: Int,
+//    val distance: Int,
+//    val fuel: Int,
+//    val money: Int,
+    val theme: Int
+)
